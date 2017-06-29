9a23b232d87206b19b2358a497e93a76e24fd722
Leonardo <leo@leovidal.es>
2017-06-29 17:00:00 +0200

    29-jun-2017

--- PRACTICES/moving-box/src/app/components/box/box.component.ts
+++ PRACTICES/moving-box/src/app/components/box/box.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit, Input, Output, OnChanges, SimpleChanges, EventEmitter } from '@angular/core';
 declare var $ :any;
 
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+// import 'rxjs/add/observable/of';
+// import 'rxjs/add/operator/delay';
+import 'rxjs/Rx';
+
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
@@ -13,26 +19,43 @@ export class BoxComponent implements OnInit, OnChanges {
     @Input() varY:number;
     @Output() eventToParent: EventEmitter<Object> = new EventEmitter<Object>();
 
-    constructor() { }
+    // array = [1,2,3,4,5];
+    // pp = Observable.from(this.array);
+        // .takeLast(3);
 
-    ngOnInit() { }
+    constructor() {
+        let tt = new Date();
+        console.log('Now!');
+        tt.setSeconds(tt.getSeconds() + 3);
+        let pp = Observable.timer(tt,1000);
+        pp.subscribe( val => {
+            console.log(val);
+        })
+
+
+     }
+
+    ngOnInit() {
+    }
 
     ngOnChanges(changes:SimpleChanges) {
+
+        this.time1[1] = new Date();
+
         if(!this.time1[0]) {
             this.time1[0] = new Date();
-            this.time1[1] = this.time1[0];
-        } else {
-            this.time1[1] = new Date();
         }
 
-        if ( (this.time1[0] - this.time1[1]) < 500 ) {
-            this.fn1();
-        } else {
-            this.time1[0] = this.time1[1];
-        }
+        // console.log(this.time1[1]-this.time1[0]);
+        this.time1[0]=this.time1[1];
+        // if ( (this.time1[0] - this.time1[1]) < 500 ) {
+        //     this.time1[0] = this.time1[1];
+        // } else {
+        //     this.fn1();
+        // }
 
 
-   }
+    }
 
     fn1() {
         let myColor  = Math.floor( Math.random() * 16777215 ).toString( 16 );
@@ -45,11 +68,12 @@ export class BoxComponent implements OnInit, OnChanges {
             backgroundColor: '#' + myColor,
             color : '#' + myColor2
         },{
-            queue    : true,
+            queue    : false,
             duration : 100
        });
     //    setTimeout(() => {this.fn1();},500);
    }
+
 
     showtIt() {
         let n = $( '#box' ).queue( 'fx' ).length;
