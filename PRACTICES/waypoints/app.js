e6af4e8388d517c7bdbd2b889ecf746628eb3794
Leonardo <leo@leovidal.es>
2017-08-01 16:54:12 +0200

    01-ago-2017

--- PRACTICES/waypoints/app.js
+++ PRACTICES/waypoints/app.js
@@ -3,28 +3,54 @@ $(document).ready(function() {
     var waypoint = $('section').waypoint({ // returns an array of all matched elements
         handler: function(direction) {
             var active_section = this.element;
-            // if (direction === "up") {
-            //     direc = 'up';
-            // } else {
-            //     direc = 'down';
-            // }
             // console.log(direction + ' ' + active_section.id);
         },
-
-        // offset: '50px'
+        // offset: '50px',
+        // offset: '25%',
+        // offset: 'bottom-in-view'
     });
+
+    // *********************************************** inview
+        var inview = new Waypoint.Inview({
+            // element: $('section')[1],
+            element: $('#section2'),
+            enter: function(direction) {
+                $('#section2').css('color','yellow');
+                console.log('Enter triggered with direction ' + direction)
+        },
+            entered: function(direction) {
+                console.log('Entered triggered with direction ' + direction)
+                $('#section2').css('color','white');
+        },
+            exit: function(direction) {
+                console.log('Exit triggered with direction ' + direction)
+        },
+            exited: function(direction) {
+                console.log('Exited triggered with direction ' + direction)
+        }
+    });
+
+    // *********************************************** sticky
+        var sticky = new Waypoint.Sticky({
+            element: $('#sticky')[0]
+        });
+
+        setTimeout(function(){
+            // sticky.destroy();
+        },3000);
+    // *******************************************************
+
+
+
 
     /*----------------------------------------------------*/
     /* Smooth Scrolling
     ------------------------------------------------------ */
        $('.smoothscroll').on('click',function (e) {
     	    e.preventDefault();
-
     	    var target = this.hash,
     	    $target = $(target);
-
             console.log($target);
-
     	    $('html, body').stop().animate({
     	        'scrollTop': $target.offset().top
     	    }, 1200, 'swing', function () {
---
+++ PRACTICES/waypoints/readme.md
@@ -0,0 +1,12 @@
+# Waypoints
+
+Waypoints is the easiest way to trigger a function when you scroll to an element.
+
+### Sticky Elements
+The Sticky Elements shortcut is used to make an element "stick" to the top of the page once a user has scrolled past it.
+### Infinite Scroll
+The Infinite Scroll shortcut transforms a traditional "Next Page" navigation into an AJAX-powered infinite scrolling UI pattern.
+### Inview
+The Inview shortcut gives you callbacks for various states of an element entering and exiting the viewport or a defined context.
+
+http://imakewebthings.com/waypoints/
