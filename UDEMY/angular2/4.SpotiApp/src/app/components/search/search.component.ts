cd3568dfeea79ef2417c1883beba65644467c647
LEO RDGZ <leo@leovidal.es>
2018-10-17 22:55:27 -0400

    Oct 17 2018

--- UDEMY/angular2/4.SpotiApp/src/app/components/search/search.component.ts
+++ UDEMY/angular2/4.SpotiApp/src/app/components/search/search.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SpotifyService } from '../../services/spotify.service';
 import { ActivatedRoute } from '@angular/router';
 import { fromEvent } from 'rxjs';
-import { debounceTime, map, switchMap, filter, finalize } from 'rxjs/operators';
+import { debounceTime, map, switchMap, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-search',
@@ -16,27 +16,6 @@ export class SearchComponent implements OnInit {
 
   constructor(private spotifyService: SpotifyService, private route: ActivatedRoute) {}
 
-  public buscar(term: String) {
-    // if (term.trim() === '') {
-    //   this.artists = [];
-    //   return;
-    // }
-    // this.loading = true;
-    // this.spotifyService.getArtist(term)
-    //   .subscribe(data => {
-    //     this.artists = data;
-    //   },
-    //     (err) => {
-    //       console.log('err', err);
-    //       this.artists = [];
-    //     },
-    //     () => { this.loading = false });
-    }
-
-    // **********************************************
-    // finalize(() => console.log('finalize'))
-    // **********************************************
-
     private startHearing() {
       const input = document.getElementById('searchInput');
       fromEvent(input, 'keyup')
@@ -49,25 +28,29 @@ export class SearchComponent implements OnInit {
           )
         )
         .subscribe(data =>
-        //   this.artists = data,
-          console.log('data', data),
+          this.artists = data,
           (err) => {
+              console.log('err', err);
               this.artists = [];
             },
             () => {
                 console.log('complete');
-            //   this.loading = false;
           })
   }
 
   ngOnInit() {
-    // focusing input
-    this.inputFocus.nativeElement.focus();
-
-    // receiving the resolve data from token.service and app.routes
-    console.log(this.route.snapshot.data);
-
-    this.startHearing();
+    this.inputFocus.nativeElement.focus(); // FOCUSING INPUT
+    console.log(this.route.snapshot.data); // SHOWING INFO COMING FROM RESOLVE IN TOKEN.SERVICE AND APP.ROUTES
+    this.startHearing(); // START LISTENING FOR KEYUP IN INPUT
   }
 
 }
+
+    // public buscar(term: String) {
+    // if (term.trim() === '') {
+    //   this.artists = [];
+    //   return;
+    // }
+    // this.loading = true;
+    // this.spotifyService.getArtist(term);
+    // }
